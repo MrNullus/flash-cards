d9f77fb08ce113e601abbb887bbb6a60a0b8867c
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-03-01 23:07:20 -0300

    upload files from the project

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+
+    <?php if ($number) { ?>
+        <a href="index.php">Clicck Here Another Number</a>
+    <?php } else { ?>
+        🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍
+    <?php } ?>
+    
+</footer>
---
+++ view/results.php
@@ -0,0 +1,29 @@
+<main class="main-results" aria-label="flash card container">
+
+    <?php
+        $cards = null;
+
+        for($i = 1; $i <= 12; $i++) {
+            $result = $number * $i;
+
+            $cards .= "
+                <div class='card'
+                    tabindex='0'
+                    arial-label={$number} multiplied by {$i} equals {$result}>
+
+                    <div class='card-front'>
+                        {$number} X {$i}
+                    </div>
+
+                    <div class='card-back' 
+                        arial-label={$number} multiplied by {$i} equals {$result}>
+                        {$result}
+                    </div>
+
+                </div>
+            ";
+        }
+
+        echo $cards;
+    ?>
+</main>
